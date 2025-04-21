4bdfd578e84d71ed53c02b8e0627af0bf813579e
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-04-21 14:10:16 +0900

    a

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -7,7 +7,22 @@ using namespace KamataEngine;
 //========================================
 
 void GameScene::Initialize() {
-	// 初期化処理
+	// テクスチャの読み込み
+	textureHandle_ = TextureManager::Load("sample.png");
+
+	// モデルの作成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// カメラのインスタンス作成
+	camera_ = new Camera();
+
+	// カメラの初期化
+	if (camera_) {
+		camera_->Initialize();
+	}
 }
 
 //========================================
@@ -23,6 +38,24 @@ void GameScene::Update() {
 //========================================
 
 void GameScene::Draw() {
-	// 描画処理
+	// DirectXCommonのインスタンスを取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// スプライト描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	// モデルの描画
+	model_->Draw(worldTransform_, *camera_, textureHandle_);
+
+	// スプライト描画後処理
+	Model::PostDraw();
 }
 
+//========================================
+// デストラクタ
+//========================================
+
+GameScene::~GameScene() {
+	// モデルの解放
+	delete model_;
+}
