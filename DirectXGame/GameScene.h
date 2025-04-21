f7fa7a6e37650178ed2a847340bce09ba6644248
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-04-21 14:10:38 +0900

    a

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -3,24 +3,28 @@
 
 class GameScene {
 public:
-	//========================================
-	// 初期化処理
-	//========================================
+	// デストラクタ
+	~GameScene();
 
 	// 初期化
 	void Initialize();
 
-	//========================================
-	// 更新処理
-	//========================================
-
 	// 更新
 	void Update();
 
-	//========================================
-	// 描画処理
-	//========================================
-
 	// 描画
 	void Draw();
+
+private:
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	// スプライト
+	KamataEngine::Model* model_ = nullptr;
+
+	// ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+
+	// カメラ
+	KamataEngine::Camera* camera_;
 };
