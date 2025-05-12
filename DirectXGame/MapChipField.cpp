b4874a1b7931eb8f254558cf409f104a246ae12e
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-05-12 15:09:14 +0900

    20

--- DirectXGame/MapChipField.cpp
+++ DirectXGame/MapChipField.cpp
@@ -12,6 +12,18 @@ std::map<std::string, MapChipType> mapChipTable = {
 };
 }
 
+MapChipType MapChipField::GetMapChipTypeByIndex(uint32_t xIndex, uint32_t yIndex) {
+	if (xIndex < 0 || kNumBlockHorizontal - 1 < xIndex) {
+		return MapChipType::kBlank;
+	}
+
+	if (yIndex < 0 || kNumBlockVertical - 1 < yIndex) {
+		return MapChipType::kBlank;
+	}
+
+    return mapChipData_.data[yIndex][xIndex];
+}
+
 void MapChipField::ResetMapChipData() {
 	mapChipData_.data.clear(); // 既存のデータをクリア
 	mapChipData_.data.resize(kNumBlockVertical);
@@ -21,28 +33,30 @@ void MapChipField::ResetMapChipData() {
 }
 
 void MapChipField::LoadMapChipCsv(const std::string& filePath) {
-ResetMapChipData();
+	ResetMapChipData();
 
-std::ifstream file;
-file.open(filePath);
-assert(file.is_open());
+	std::ifstream file;
+	file.open(filePath);
+	assert(file.is_open());
 
-std::stringstream mapChipCsv;
+	std::stringstream mapChipCsv;
 
-mapChipCsv << file.rdbuf();
+	mapChipCsv << file.rdbuf();
 
-file.close();
+	file.close();
 
-for (uint32_t i = 0; i < kNumBlockVertical; ++i) {
-	std::string line;
-	getline(mapChipCsv, line);
-	std::stringstream lineStream(line);
-	for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
-		std::string word;
-		getline(lineStream, word, ',');
-		if (mapChipTable.contains(word)) {
-			mapChipData_.data[i][j] = mapChipTable[word];
+	for (uint32_t i = 0; i < kNumBlockVertical; ++i) {
+		std::string line;
+		getline(mapChipCsv, line);
+		std::stringstream lineStream(line);
+		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+			std::string word;
+			getline(lineStream, word, ',');
+			if (mapChipTable.contains(word)) {
+				mapChipData_.data[i][j] = mapChipTable[word];
+			}
 		}
 	}
-
 }
+
+Vector3 MapChipField::GetMapPositionTypeByIndex(uint32_t xIndex, uint32_t yIndex) { return Vector3(kBlockWidth * xIndex, kBlockHeight * (kNumBlockVertical - 1 - yIndex), 0); }
--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -1,22 +1,29 @@
-#pragma once
-#include <cstdint>
-#include <vector>
+#pragma once  
+#include <cstdint>  
+#include <vector>  
+#include <KamataEngine.h>
 
-class MapChipField {
-	static inline const float kBlockWidth = 1.0f; // ブロック1個分の横幅
-	static inline const float kBlockHeight = 1.0f; // ブロック1個分の縦幅
-	static inline const uint32_t kNumBlockVertical = 20; // 縦のブロック数
-	static inline const uint32_t kNumBlockHorizontal = 20; // 横のブロック数
-	MapChipData mapChipData_;                              // マップチップデータ
-	void ResetMapChipData();                               // マップチップデータのリセット
-	void LoadMapChipCsv(const std::string& filePath);     // マップチップデータの読み込み
-};
+using namespace KamataEngine;
 
 enum class MapChipType {
 	kBlank, // 空白
 	kBlock, // ブロック
-};
+};  
 
 struct MapChipData {
 	std::vector<std::vector<MapChipType>> data;
+};
+
+class MapChipField {
+	MapChipData mapChipData_;                                            // マップチップデータ
+	MapChipType GetMapChipTypeByIndex(uint32_t xIndex, uint32_t yIndex); // 指定したインデックスのマップチップデータを取得する
+	void ResetMapChipData();                                             // マップチップデータのリセット
+	void LoadMapChipCsv(const std::string& filePath);                    // マップチップデータの読み込み
+	Vector3 GetMapPositionTypeByIndex(uint32_t xIndex, uint32_t yIndex);
+
+private:
+	static inline const float kBlockWidth = 1.0f;          // ブロック1個分の横幅
+	static inline const float kBlockHeight = 1.0f;         // ブロック1個分の縦幅
+	static inline const uint32_t kNumBlockVertical = 20;   // 縦のブロック数
+	static inline const uint32_t kNumBlockHorizontal = 20; // 横のブロック数
 };
