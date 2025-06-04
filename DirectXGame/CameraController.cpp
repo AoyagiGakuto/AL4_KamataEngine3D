e53eeaec5c3b39511592aee982474c3c5be0ce72
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-06-04 15:15:11 +0900

    a

--- DirectXGame/CameraController.cpp
+++ DirectXGame/CameraController.cpp
@@ -4,8 +4,10 @@
 using namespace KamataEngine;
 
 void CameraController::Initialize() {
-
+	camera_.translation_ = target_->GetWorldTransform().translation_ + targetOffset_;
+	camera_.UpdateMatrix();
 }
+
 void CameraController::Update() {
 	const WorldTransform& targetWorldTransform = target_->GetWorldTransform();
 	camera_.translation_ = targetWorldTransform.translation_ + targetOffset_;
